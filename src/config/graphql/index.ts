6f4f2009bab0c9319850092f4e9ce333193544ab
Franck COLOMBO <franck.colombo@cnav.fr>
2023-02-24 13:44:31 +0100

    :bug: (module) remove import data-config

--- src/config/graphql/index.ts
+++ src/config/graphql/index.ts
@@ -1,14 +1,13 @@
-import { GraphQLConfig } from 'src/config/graphql.config';
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { GqlModuleAsyncOptions, GqlModuleOptions, GqlOptionsFactory } from "@nestjs/graphql";
+import { ApolloDriver, } from "@nestjs/apollo";
+import { GqlModuleOptions } from "@nestjs/graphql";
 
 
 
 
-export const GraphQLConfigService : GqlModuleOptions=  {
-    
-        driver: ApolloDriver,
-        autoSchemaFile: 'schema.gql',
-      
+export const GraphQLConfigService: GqlModuleOptions = {
+
+  driver: ApolloDriver,
+  autoSchemaFile: 'schema.gql',
+
 }
 
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,7 @@ import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/models/user.model';
-import { GraphQLModule } from '@nestjs/graphql';
-import { GraphQLConfig } from 'src/config/graphql.config';
+
 
 @Module({
   imports: [
