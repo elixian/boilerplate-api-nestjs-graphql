f054ec9ac851f25ab3f9fbde17f2b6ca2fa2e2ea
Franck COLOMBO <franck.colombo@cnav.fr>
2023-03-08 18:52:23 +0100

    :sparkles: (jwt) feat ok return accesToken

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthMutationResolver } from './resolvers/auth.mutation.resolver';
 import { UserModule } from './../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport/dist';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt/dist';
 
 
 @Module({
-  imports: [UserModule,PassportModule],
+  imports: [UserModule,PassportModule,
+    JwtModule.registerAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:(configService :ConfigService)=>({
+        secret: configService.get('JWT_SECRET'),
+        signOptions:{expiresIn: configService.get('JWT_EXPIRE_TIME')}
+      })
+    })
+  ],
   providers: [AuthService, AuthMutationResolver, LocalStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,19 @@ import { UserEntity } from 'src/user/models/user.model';
 import { UserService } from './../user/user.service';
 import { Injectable } from '@nestjs/common';
 import { AuthLoginOutPut } from './dto/auth.login.dto';
+import { JwtService } from '@nestjs/jwt';
 
+
+interface JWTPayload{
+  username,
+  sub
+}
 @Injectable()
 export class AuthService {
-  constructor(private userService : UserService) {}
+  constructor(
+    private userService : UserService,
+    private jwtService: JwtService
+    ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.getUser(email);
@@ -17,8 +26,9 @@ export class AuthService {
   }
 
   async login(user: UserEntity): Promise<AuthLoginOutPut>{
+    const payload: JWTPayload = { username: user.name, sub: user.ID };
     return {
-      accessToken : 'fake_token'
+      accessToken : this.jwtService.sign(payload),
     }
   }
 }
